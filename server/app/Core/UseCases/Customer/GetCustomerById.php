ff53ab7f9ee6a62da8dee71c0b531e3f7625c788
Mateus Soares <mateussoares114@gmail.com>
2024-11-28 05:59:16 -0300

    refactor: using ThrowException trait

--- server/app/Core/UseCases/Customer/GetCustomerById.php
+++ server/app/Core/UseCases/Customer/GetCustomerById.php
@@ -4,13 +4,20 @@ namespace App\Core\UseCases\Customer;
 
 use App\Core\Contracts\Repositories\CustomerRepositoryInterface;
 use App\Core\Domain\Entities\Customer;
+use App\Support\Traits\ThrowException;
 
 class GetCustomerById
 {
+    use ThrowException;
+
     public function __construct(private CustomerRepositoryInterface $customerRepository) {}
 
     public function execute(int $id): ?Customer
     {
-        return $this->customerRepository->findById($id);
+        $customer = $this->customerRepository->findById($id);
+
+        if (!$customer) $this->throwNotFoundException('Customer not found');
+
+        return $customer;
     }
 }
