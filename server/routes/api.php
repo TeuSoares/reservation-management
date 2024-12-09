6cd001471e894298a06615de465592796623446d
Mateus Soares <mateussoares114@gmail.com>
2024-12-09 06:19:44 -0300

    refactor: add new routes and use check.permission middleware

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('customers', CustomerController::class)->parameter('customers', 'id');
+Route::get('customers/check-record', [CustomerController::class, 'checkRecord']);
+Route::post('customers', [CustomerController::class, 'store']);
+Route::post('customers/check-code', [CustomerController::class, 'checkCode']);
+
+Route::apiResource('customers', CustomerController::class)
+    ->middleware(['check.permission'])
+    ->parameter('customers', 'id')
+    ->except('store');
