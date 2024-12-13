3e18fed66517f25a2b4ce9d987e69a1909e809d2
Mateus Soares <mateussoares114@gmail.com>
2024-12-13 06:25:21 -0300

    feat: add store customer method

--- server/app/Http/Controllers/CustomerController.php
+++ server/app/Http/Controllers/CustomerController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Core\UseCases\Customer\CreateCustomerUseCase;
 use App\Core\UseCases\Customer\GetAllCustomers;
 use App\Core\UseCases\Customer\GetCustomerById;
+use App\Http\Requests\Customer\StoreCustomerRequest;
 use App\Support\HttpResponse;
 use Illuminate\Http\JsonResponse;
 
@@ -12,6 +14,7 @@ class CustomerController extends Controller
     public function __construct(
         private GetAllCustomers $getAllCustomersUseCase,
         private GetCustomerById $getCustomerByIdUseCase,
+        private CreateCustomerUseCase $createCustomerUseCase,
         private HttpResponse $httpResponse
     ) {}
 
@@ -23,5 +26,11 @@ class CustomerController extends Controller
     public function show(int $id): JsonResponse
     {
         return $this->httpResponse->data($this->getCustomerByIdUseCase->execute($id));
+    }
+
+    public function store(StoreCustomerRequest $request): JsonResponse
+    {
+        $message = $this->createCustomerUseCase->execute($request->all());
+        return $this->httpResponse->message($message, 201);
     }
 }
