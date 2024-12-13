729452003cf2d54838062e6fc0aff6d5af3ba219
Mateus Soares <mateussoares114@gmail.com>
2024-12-13 06:25:59 -0300

    refactor: add name to routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('customers/check-record', [CustomerController::class, 'checkRecord']);
-Route::post('customers', [CustomerController::class, 'store']);
-Route::post('customers/check-code', [CustomerController::class, 'checkCode']);
+Route::get('customers/check-record', [CustomerController::class, 'checkRecord'])->name('customers.checkRecord');
+Route::post('customers/check-code', [CustomerController::class, 'checkCode'])->name('customers.checkCode');
+Route::post('customers', [CustomerController::class, 'store'])->name('customers.store');
 
 Route::apiResource('customers', CustomerController::class)
     ->middleware(['check.permission'])
