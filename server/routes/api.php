066aa6df242bb464ee4c0de679ca2ebd3275ece4
Mateus Soares <mateussoares114@gmail.com>
2024-12-25 09:52:27 -0300

    refactor: change get method to post method

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('customers/check-record', [CustomerController::class, 'checkRecord'])->name('customers.checkRecord');
-Route::post('customers/check-code', [CustomerController::class, 'checkCode'])->name('customers.checkCode');
+Route::post('customers/check-record', [CustomerController::class, 'checkRecord'])->name('customers.check-record');
+Route::post('customers/check-code', [CustomerController::class, 'checkCode'])->name('customers.check-code');
 Route::post('customers', [CustomerController::class, 'store'])->name('customers.store');
 
 Route::apiResource('customers', CustomerController::class)
