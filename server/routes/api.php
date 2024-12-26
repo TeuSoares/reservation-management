2780ef467cbf65a1441cb453277586ed3a802eb2
Mateus Soares <mateussoares114@gmail.com>
2024-12-26 06:03:43 -0300

    refactor: change routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,11 +3,22 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('customers/check-record', [CustomerController::class, 'checkRecord'])->name('customers.check-record');
-Route::post('customers/check-code', [CustomerController::class, 'checkCode'])->name('customers.check-code');
-Route::post('customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::group([
+    'middleware' => ['check.origin'],
+], function () {
+    Route::group(['prefix' => 'customers'], function () {
+        Route::post('check-record', [CustomerController::class, 'checkRecord'])->name('customers.check-record');
+        Route::post('check-code', [CustomerController::class, 'checkCode'])->name('customers.check-code');
+        Route::post('', [CustomerController::class, 'store'])->name('customers.store');
 
-Route::apiResource('customers', CustomerController::class)
-    ->middleware(['check.permission'])
-    ->parameter('customers', 'id')
-    ->except('store');
+        Route::group(['middleware' => ['auth:sanctum']], function () {
+            Route::get('', [CustomerController::class, 'index'])->name('customers.index');
+            Route::delete('{id}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+        });
+
+        Route::group(['middleware' => ['check.permission']], function () {
+            Route::get('{id}', [CustomerController::class, 'show'])->name('customers.show');
+            Route::put('{id}', [CustomerController::class, 'update'])->name('customers.update');
+        });
+    });
+});
