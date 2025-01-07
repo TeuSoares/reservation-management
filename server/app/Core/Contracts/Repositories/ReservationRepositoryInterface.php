7edca2587ec0ba47f97bdab9ae49070875c0ce11
Mateus Soares <mateussoares114@gmail.com>
2025-01-07 05:37:58 -0300

    refactor: add create, update, delete method and customer repository method entity

--- server/app/Core/Contracts/Repositories/ReservationRepositoryInterface.php
+++ server/app/Core/Contracts/Repositories/ReservationRepositoryInterface.php
@@ -8,4 +8,7 @@ interface ReservationRepositoryInterface extends RepositoryInterface
 {
     public function getAll(object $params): object;
     public function findById(int $id): ?Reservation;
+    public function create(array $data): Reservation;
+    public function update(int $id, array $data): void;
+    public function delete(int $id): void;
 }
--- server/app/Infrastructure/Persistence/Repositories/ReservationRepository.php
+++ server/app/Infrastructure/Persistence/Repositories/ReservationRepository.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use App\Core\Contracts\Repositories\ReservationRepositoryInterface;
 use App\Core\Domain\Entities\Reservation as ReservationEntity;
 use App\Infrastructure\Persistence\Models\Reservation;
+use Carbon\Carbon;
 
 class ReservationRepository implements ReservationRepositoryInterface
 {
     public function __construct(
-        private Reservation $model
+        private Reservation $model,
+        private CustomerRepository $customerRepository
     ) {}
 
     public function getAll(object $params): object
@@ -52,15 +54,36 @@ class ReservationRepository implements ReservationRepositoryInterface
         return $reservation ? $this->toEntity($reservation) : null;
     }
 
+    public function create(array $data): ReservationEntity
+    {
+        $reservation = $this->model->create($data);
+
+        return $this->toEntity($reservation);
+    }
+
+    public function update(int $id, array $data): void
+    {
+        $reservation = $this->model->findOrFail($id);
+        $reservation->update($data);
+    }
+
+    public function delete(int $id): void
+    {
+        $this->model->findOrFail($id)->delete();
+    }
+
     public function toEntity(Model $model): ReservationEntity
     {
+        $customerEntity = $this->customerRepository->toEntity($model->customer);
+
         return new ReservationEntity(
             $model->id,
             $model->customer_id,
-            $model->booking_date,
+            Carbon::parse($model->booking_date),
             $model->number_people,
             $model->payment_confirmed,
-            $model->canceled
+            $model->canceled,
+            $customerEntity
         );
     }
 }
