a1856812b765ecc614ff6136ab503159e9d43d20
Mateus Soares <mateussoares114@gmail.com>
2025-01-07 06:18:45 -0300

    refactor: add verify date and number of people

--- server/app/Http/Requests/Reservation/MutationReservationRequest.php
+++ server/app/Http/Requests/Reservation/MutationReservationRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Reservation;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class MutationReservationRequest extends FormRequest
@@ -15,8 +16,10 @@ class MutationReservationRequest extends FormRequest
     {
         $rules = [
             'customer_id' => 'required|integer|exists:customers,id',
-            'booking_date' => 'sometimes|required|date',
-            'number_people' => 'sometimes|required|integer',
+            'booking_date' => 'sometimes|required|date' .
+                '|after_or_equal:' . Carbon::now()->format('Y-m-d') .
+                '|before_or_equal:' . Carbon::now()->addMonths(1)->format('Y-m-d'),
+            'number_people' => 'sometimes|required|integer|min:1',
         ];
 
         if (request()->isMethod('POST')) {
@@ -34,7 +37,10 @@ class MutationReservationRequest extends FormRequest
             'customer_id.integer' => 'Customer ID must be an integer',
             'customer_id.exists' => 'Customer ID does not exist',
             'booking_date.date' => 'Booking date must be a valid date',
+            'booking_date.after' => 'Booking date must be today or later',
+            'booking_date.before' => 'The reservation date must be next month at the latest',
             'number_people.integer' => 'Number of people must be an integer',
+            'number_people.min' => 'Number of people must be at least 1',
         ];
     }
 }
