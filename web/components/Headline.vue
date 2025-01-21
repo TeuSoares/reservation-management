63b99a4223cfbba98c6990a7dc3681f839914d6d
Mateus Soares <mateussoares114@gmail.com>
2025-01-21 06:16:35 -0300

    feat: add Headline component

---
+++ web/components/Headline.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1
+    :class="[
+      `text-${align}`,
+      `text-${color}`,
+      'text-ellipsis text-balance text-xl font-[700] leading-[1.5]',
+    ]"
+  >
+    {{ text }}
+  </h1>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: "black",
+  },
+  align: {
+    type: String,
+    default: "left",
+    validator: (value: string) =>
+      ["left", "center", "right", "justify"].includes(value),
+  },
+});
+</script>
