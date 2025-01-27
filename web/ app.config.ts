c5d14fa702356463b4c28accadb914751308e637
Mateus Soares <mateussoares114@gmail.com>
2025-01-27 06:18:38 -0300

    chore: app config setup

---
+++ web/ app.config.ts
@@ -0,0 +1,19 @@
+export default defineAppConfig({
+  ui: {
+    button: {
+      default: {
+        size: "md",
+        color: "gray",
+        variant: "ghost",
+      },
+      color: {
+        red: "#802b2b",
+      },
+    },
+    input: {
+      color: {
+        red: "#802b2b",
+      },
+    },
+  },
+});
