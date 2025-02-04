77e89a9d81675cc7831f07c82479d07ffa087140
Mateus Soares <mateussoares114@gmail.com>
2025-02-04 06:15:03 -0300

    chore: add pinia

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/icon"],
+  modules: ["@nuxt/ui", "@nuxt/icon", "@pinia/nuxt"],
   tailwindcss: {
     cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
     config: {},
     viewer: true,
     exposeConfig: false,
+  },
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL,
+    },
   },
 });
