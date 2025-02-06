dd22eb8a387c3c040a9a51f6c3f126ed8c9a39ff
Mateus Soares <mateussoares114@gmail.com>
2025-02-06 06:17:35 -0300

    feat: add thanks page

---
+++ web/pages/reservation/thanks.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <UCard style="width: 600px" class="text-center">
+      <Icon name="uil:check" size="50" class="text-main" />
+      <h1 class="text-xl font-bold text-main">Reservation made successfully</h1>
+      <p>Thanks for your reservation</p>
+    </UCard>
+  </div>
+</template>
